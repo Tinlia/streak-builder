79fa2f054e96d5911697dbf4b39b95a23b8f0933
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2024-01-07 21:40:15 +0000

    Move checkstreak to bg, Add Gems, Optimize code

--- popup.js
+++ popup.js
@@ -1,21 +1,22 @@
-var currentDomain = "";
-var domainPath = "";
-var currentDate = new Date();
-var dateNum = 0
+var currentDomain = "" , domainPath = "";
+var dateNum = new Date().getDate();
 var streakLen, maxStreak, lastVisit;
 var streakInfoArray = [streakLen, maxStreak, lastVisit];
-var lightMode;
+var lightMode, gemCount = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log("[Creating event listeners...]");
+  createEventListeners();
+  chrome.storage.local.get('gems', function(result) {
+    gemCount = result.gems;
+    document.getElementById('gems').innerHTML = "💎 " + gemCount;
+  });
   console.log("[Setting lightMode...]"); 
   setLightMode().then(() => {
     console.log("[Getting current tab...]");
     getTab();
     console.log("[Changing colour...]");
     changeColor(true);
-    console.log("[Creating event listeners...]");
-    dateNum = currentDate.getDate();
-    createEventListeners();
   });
 });
 
@@ -70,34 +71,18 @@ function getTab(){
 function checkStreak(){
   chrome.storage.local.get(currentDomain, function(result) {
     var streakDetails = result[currentDomain];
-    if(streakDetails == null || currentDomain.includes(null)){
+    if(streakDetails == null || streakDetails == undefined){
       showStartStreakButton()
-      document.getElementById('newStreak').addEventListener('click', function() {
-        streakInfoArray = [1, 1, dateNum]
-        var obj = {};
-        obj[currentDomain] = JSON.stringify(streakInfoArray);
-        chrome.storage.local.set(obj, function() {
-          checkStreak();
-        });
-      })
+      document.getElementById('newStreak').addEventListener('click', () => createNewStreak());
     }
     else{
-      [streakLen, maxStreak, lastVisit] = JSON.parse(streakDetails);
-      var differenceInDays = dateNum - lastVisit;
-      if(differenceInDays == 0){streakLen += 0;}
-      else if(differenceInDays == 1){streakLen += 1;}
-      else if(differenceInDays <= -29 && dateNum == 1){streakLen +=1;}
-      else{streakLen = 1;}
-      lastVisit = dateNum;
-      if(streakLen > maxStreak){maxStreak = streakLen;}
+      [streakLen, maxStreak, lastVisit] = streakDetails;
       showStreakInfo();
-      streakInfoArray = [streakLen, maxStreak, lastVisit]
+      streakInfoArray = [streakLen, Math.max(streakLen,maxStreak), dateNum]; 
       var obj = {};
-      obj[currentDomain] = JSON.stringify(streakInfoArray);
+      obj[currentDomain] = streakInfoArray;
       chrome.storage.local.set(obj);
-      document.getElementById('deleteStreak').addEventListener('click', function() {
-        showDeleteStreakButtons(false);
-      })
+      document.getElementById('deleteStreak').addEventListener('click', () => showDeleteStreakButtons(false));
     }
   });
 }
@@ -124,14 +109,16 @@ function changeColor(firstTime){
   LDModeButton.innerHTML = lightMode?'☀️': '🌙';
 }
 
+// Show streak info
 function showStreakInfo(){
   document.getElementById('streak').innerHTML = `
       <p>Current Streak: </br>🔥 ` + streakLen + ` </br>
-      <p>Max Streak: </br>🔥 ` + maxStreak + `</p></br>
+      <p>Max Streak: </br>🔥 ` + maxStreak + `</p><br>
       <button id="deleteStreak" type="button">🗑️</button>` 
       + `${domainPath.includes('/Tinlia/')?"</br></br><i>Thanks for checking out my work!</i>":""}`;
 }
 
+// Button Displayers
 function showStartStreakButton(){
   document.getElementById('streak').innerHTML = `<button id="newStreak" type="button"><b>+</b> Start a 🔥Streak</button>`;
 }
@@ -147,12 +134,11 @@ function showDeleteStreakButtons(deleteAll) {
     if(deleteAll) {
       chrome.storage.local.clear(function() {
         var error = chrome.runtime.lastError;
-        if (error) {
-          console.error(error);
-        } else {
+        if (error) { console.error(error); } 
+        else {
           console.log("Deletion complete!");
-          window.close(); 
-          checkStreak();
+          chrome.storage.local.set({'gems': gemCount}); // Gems persist through deletion
+          window.close(); // Close popup
         }
       });
     } else {
@@ -168,10 +154,19 @@ function showDeleteStreakButtons(deleteAll) {
     }
   })
 
+  // Cancel Delete Listener
   document.getElementById('deleteStreakCancel').addEventListener('click', function() {
     console.log("Streak Deletion Cancelled!");
     getTab();
   })
+}
+
+// Create a new streak
+function createNewStreak(){
+  streakInfoArray = [1, 1, dateNum]
+  var obj = {};
+  obj[currentDomain] = streakInfoArray;
+  chrome.storage.local.set(obj, () => checkStreak());
 }
 
 function createEventListeners(){
